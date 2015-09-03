a14eab717084b660689c2f95ccab6e0e3c95c634
Ashlin Aronin <ashlinaronin@gmail.com>
2015-09-02 17:00:32 -0700

    Update coach active project routes

--- routes/coach_active_project.php
+++ routes/coach_active_project.php
@@ -13,7 +13,7 @@
     /* 2. Show next un-completed step in this project.
     ** Allow user to check it off or choose that they don't have time.
     ** Ask user, are you confident that you can complete this step today? */
-    $coach_active_project->post('/{id}/next_step', function($id) use ($app) {
+    $coach_active_project->get('/{id}/next_step', function($id) use ($app) {
         $project = Project::find($id);
 
         return $app['twig']->render('coach/active_project/2next_step.html.twig', array(
@@ -27,7 +27,7 @@
     ** If no, link to add a step. If yes, link to /{id}/next_step.
     ** This route will handle its own post data.
     **/
-    $coach_active_project->post('/{id}/reorder_steps', function($id) use ($app) {
+    $coach_active_project->get('/{id}/reorder_steps', function($id) use ($app) {
         $project = Project::find($id);
 
         return $app['twig']->render('coach/active_project/3reorder_steps.html.twig', array(
@@ -42,7 +42,7 @@
     $coach_active_project->post('/{id}/add_step', function($id) use ($app) {
         $project = Project::find($id);
 
-        return $app['twig']->render('coach/active_project/4reorder_steps.html.twig', array(
+        return $app['twig']->render('coach/active_project/4add_step.html.twig', array(
             'project' => $project
         ));
     });
@@ -54,6 +54,16 @@
     ** Let user choose if they'd like to see the next step -> /next_step
     ** or had enough for the day -> /enough
     */
+    $coach_active_project->get('/{id}/complete', function($id) use ($app) {
+        $project = Project::find($id);
+
+        return $app['twig']->render('coach/active_project/5complete.html.twig', array(
+            'project' => $project
+        ));
+    });
+
+    // Duplicate for post, sent here if user checks that they have finished step
+    // on get page
     $coach_active_project->post('/{id}/complete', function($id) use ($app) {
         $project = Project::find($id);
 
