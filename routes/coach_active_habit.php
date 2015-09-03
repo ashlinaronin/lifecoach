433b375d40307e15c0e15ba45689b50e591dabdb
Mitch Rivet <student@epicodus.com>
2015-09-03 16:01:34 -0700

    add no and route to active habits route

--- routes/coach_active_habit.php
+++ routes/coach_active_habit.php
@@ -14,10 +14,21 @@
     $coach_active_habit->post('/success/{id}', function($id) use ($app) {
       $habit = Habit::find($id);
       $habit->completeOnDayId($habit->getId());
-      
+
       $days_complete = $habit->getDaysCompleted($habit->getId());
       return $app['twig']->render('coach/active_habit/2success.html.twig', array(
         'habit' => $habit,
+        'days' => $days_complete
+      ));
+    });
+
+    $coach_active_habit->get('/fail/{id}', function($id) use ($app) {
+      $habit = Habit::find($id);
+      $motivation = $habit->getMotivation();
+      $days_complete = $habit->getDaysCompleted($habit->getId());
+      return $app['twig']->render('coach/active_habit/3failure.html.twig', array(
+        'habit' => $habit,
+        'motivation' => $motivation,
         'days' => $days_complete
       ));
     });
